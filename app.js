6b632acfe7f83c6d58295075c4c15976c42a7b17
Jhaanvi Golani <jgolani2@scu.edu>
2022-09-30 20:09:28 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi, I am Jhaanvi!'));
+app.get('/', (req, res) => res.send('Hi, I am back and testing the code pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
